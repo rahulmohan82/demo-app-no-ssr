6f55fd88f8e24a1d8488e2611d6000cf5ebb333d
rahulmohan82 <rahulmohan82@gmail.com>
2018-02-23 17:35:49 +0530

    my first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+;
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<div id="app-root-browser">
+                <router-outlet></router-outlet>
+            </div>`,
 })
 export class AppComponent {
-  title = 'app';
+
+
+
+  constructor() {
+
+  }
+
+
+
 }
